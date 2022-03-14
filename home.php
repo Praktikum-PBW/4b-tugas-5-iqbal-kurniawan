8729add09006c68fc0279592570fbcbaaf631bb1
Iqbal Kurniawan <99189172+Baleeeee@users.noreply.github.com>
2022-03-14 18:53:16 +0700

    Tugas Pertemuan 5

---
+++ home.php
@@ -0,0 +1,67 @@
+<html>
+    <head>
+        <title>testo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <nav id="main-navbar" class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg fixed-top">   
+     <div class="container-fluid satu">
+      <a class="navbar-brand" ><img src="https://images.unsplash.com/photo-1634034379073-f689b460a3fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80" alt="" width="35" height="29" class="d-inline-block align-text-top">Baleeee</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active " aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link px-5" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="kontak.php">Contact us</a>
+        </li>
+       </ul>
+      </div>
+     </div>
+    </nav>
+    <div class="container-fluid banner">
+      <div class="container text-center">
+        <h4 class="display-6">Selamat Datang</h4>
+        <h3 class="display-1">Halo Guys!</h3>
+        <a href="about.php">
+          <button type="button" class="btn btn-danger btn-lg mt-3">Cek Profile</button>
+        </a>
+        <a href="kontak.php">
+          <button type="button" class="btn btn-danger btn-lg mt-3">Hubungi Kami</button>
+        </a>
+      </div>
+    </div>
+    <div class="container-fluid pt-5 pb-5">
+      <div class="container text-center">
+        <h2 class="display-3" >Halaman</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
+        <div class="row pt-4">
+          <div class="col-md-4">
+            <span class="lingkaran"><i class="fas fa-code fa-5x"></i></span>
+            <h3 class="mt-3">Website</h3>
+            <p>Lorem Ipsum is simply dummy text </p>
+          </div>
+          <div class="col-md-4">
+            <span class="lingkaran"><i class="fas fa-laptop fa-5x"></i></span>
+            <h3 class="mt-3">Aplikasi</h3>
+            <p>Lorem Ipsum is simply dummy text </p>
+          </div>
+          <div class="col-md-4">
+            <span class="lingkaran"><i class="fas fa-palette fa-5x"></i></span>
+            <h3 class="mt-3">Desain</h3>
+            <p>Lorem Ipsum is simply dummy text </p>
+          </div>
+        </div>
+      </div>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+</html>
